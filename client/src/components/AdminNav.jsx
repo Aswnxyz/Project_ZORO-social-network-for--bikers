25714133a804ee8e3488b80914582d41c718969a
Aswin <aswinanith007@gmail.com>
2024-02-09 15:49:12 +0530

    Removed unused img tage

--- client/src/components/AdminNav.jsx
+++ client/src/components/AdminNav.jsx
@@ -9,7 +9,6 @@ const AdminNav = () => {
       <div className="flex items-center justify-start rtl:justify-end">
       
         <a href="" className="flex ms-2 md:me-24">
-          {/* <img src="" className="h-8 me-3" alt="" /> */}
           <span className="self-center text-xl font-semibold sm:text-2xl whitespace-nowrap dark:text-white">ZORO</span>
         </a>
       </div>
