19eb1ce2457dcdabf78932de2555911ce622dc2f
Aswin <aswinanith007@gmail.com>
2024-02-09 14:52:29 +0530

    Canceled last change

--- messages/src/index.js
+++ messages/src/index.js
@@ -32,7 +32,7 @@ const startServer = async () => {
 
   httpServer
     .listen(PORT, () => {
-      console.log(`listening to port ${PORT}...`);
+      console.log(`listening to port ${PORT}`);
     })
     .on("error", (err) => {
       console.log(err);
