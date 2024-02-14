3bab936d0d1991c5744527ecc9def6ebdd4037d7
Aswin <aswinanith007@gmail.com>
2024-02-14 15:34:27 +0530

    testing socket problem

--- notification/src/api/notification.js
+++ notification/src/api/notification.js
@@ -1,13 +1,13 @@
 const NotificationService = require("../services/notification-service");
 const { SubscribeMessage } = require("../utils");
-const userAuth = require("./middlewares/auth")
+const userAuth = require("./middlewares/auth");
 const express = require("express");
-module.exports = (app,channel,io) => {
+module.exports = (app, channel, io) => {
   const service = new NotificationService(io);
-    SubscribeMessage(channel, service);
-    const router = express.Router();
+  SubscribeMessage(channel, service);
+  const router = express.Router();
 
-  io.of("/api/notification").on("connection", (socket) => {
+  io.of("/notification").on("connection", (socket) => {
     console.log("User connected to socket");
 
     socket.on("registerUser", async (userId) => {
@@ -21,8 +21,6 @@ module.exports = (app,channel,io) => {
         );
         io.to(userId).emit("unreadNotifications", unreadNotifications);
 
-        
-
         io.to(userId).emit("connected");
       } catch (error) {
         console.error(`Error getting unread notifications: ${error.message}`);
@@ -30,18 +28,17 @@ module.exports = (app,channel,io) => {
     });
 
     // Other event handlers...
-    socket.on("clearUnreadNotifications",async (userId) => {
+    socket.on("clearUnreadNotifications", async (userId) => {
       // Clear unread notifications for the user
       try {
-          await service.clearUnreadNotifications(userId);
-      const unreadNotifications = await service.getUnreadNotifications(
+        await service.clearUnreadNotifications(userId);
+        const unreadNotifications = await service.getUnreadNotifications(
           userId
         );
         io.to(userId).emit("unreadNotifications", unreadNotifications);
       } catch (error) {
-        
+        console.log(error);
       }
-    
     });
 
     // Disconnect event (optional)
@@ -50,17 +47,14 @@ module.exports = (app,channel,io) => {
     // });
   });
 
-    router.get('/getNotifications',userAuth,async(req,res,next)=>{
-        try {
-          const data = await service.getNotifications(req.user._id);
-          return res.status(200).json(data)
-        } catch (error) {
-          next(error)
-        }
+  router.get("/getNotifications", userAuth, async (req, res, next) => {
+    try {
+      const data = await service.getNotifications(req.user._id);
+      return res.status(200).json(data);
+    } catch (error) {
+      next(error);
+    }
+  });
 
-    })
-    
-
-    app.use("/api/notification", router);
-
-}
+  app.use("/notification", router);
+};
